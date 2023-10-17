5b85d4120e46fd190cf022864ccbd01de5545f56
vlademir-junior <vlademir.junior.nh@gmail.com.br>
2023-10-17 12:10:59 -0300

    Resolvendo problemas de layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-function Card({ data, type }) {
+export default function Card({ data }) {
   const [favorited, setFavorited] = useState(false);
 
   useEffect(() => {
@@ -25,20 +24,46 @@ function Card({ data, type }) {
 
   return (
     <div className={`col-md-4 mb-4`}>
-      <div className={`card`}>
+      <div className={`card my-card`}>
         <div className="card-body">
           <h3 className="card-title">{data.titulo}</h3>
           <p className="card-text">{data.introducao}</p>
-          <Link to={`/${type === 'release' ? 'releases' : 'noticias'}/${data.id}`} className="btn btn-primary">
-            Ler mais
-          </Link>
-          <button onClick={toggleFavorite} className={`btn btn-link`}>
-            {favorited ? <i className="bi bi-heart-fill"></i> : <i className="bi bi-heart"></i>}
+          <p className="card-text">
+            <strong>Data de Publicação:</strong> {data.data_publicacao}
+          </p>
+          <p className="card-text">
+            <strong>Tipo:</strong> {data.tipo}
+          </p>
+          <p className="card-text">
+            <strong>Editorias:</strong> {data.editorias}
+          </p>
+          {data.destaque && (
+            <p className="card-text">
+              <strong>Destaque:</strong> Sim
+            </p>
+          )}
+          {data.link && (
+            <a
+            href={data.link}
+            className="btn btn-outline-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Veja a notícia completa
+          </a>          
+          )}
+          <button
+            onClick={toggleFavorite}
+            className={`btn btn-link text-danger`}
+          >
+            {favorited ? (
+              <i className="bi bi-heart-fill"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
           </button>
         </div>
       </div>
     </div>
   );
 }
-
-export default Card;
